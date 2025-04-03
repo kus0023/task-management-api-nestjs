f6465d4002a97207cc708250e18c9b1012f22718
kus0023 <utsav.singh0023@gmail.com>
2025-04-03 16:48:32 +0530

    Task resource is cleaned up and updated the update method of task service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 
@@ -11,14 +11,17 @@ export class TasksService {
   async create(createTaskDto: Prisma.TaskCreateManyAssignedToDetailInput, currentUserId: string) {
 
     const boardListExists = await this.dbService.boardList
-      .findUnique({ where: { id: createTaskDto.boardListId, }, include: { boardDetail: true } });
+      .findUnique({
+        where: { id: createTaskDto.boardListId, },
+        include: { boardDetail: true }
+      });
 
     if (!boardListExists) {
-      throw new Error('Board list does not exist');
+      throw new NotFoundException('Board list does not exist');
     }
 
     if (boardListExists.boardDetail.userId !== currentUserId) {
-      throw new Error('Board does not belong to user');
+      throw new UnauthorizedException('Board does not belong to user');
     }
 
 
@@ -38,7 +41,7 @@ export class TasksService {
     });
   }
 
-  findOne(id: string, currentUserId: string) {
+  async findOne(id: string, currentUserId: string) {
     return this.dbService.task.findUnique({
       where: {
         id: id, boardListDetail: {
@@ -57,31 +60,84 @@ export class TasksService {
     });
   }
 
-  update(id: string, updateTaskDto: Prisma.TaskUpdateInput, currentUserId: string) {
-    return this.dbService.task.update({
-      where: {
-        id: id,
-        boardListDetail: {
-          boardDetail: {
-            userId: currentUserId
-          }
-        }
-      },
-      data: {
-        ...updateTaskDto,
-        deadline: updateTaskDto.deadline && new Date(updateTaskDto.deadline as string)
-      },
+  async update(id: string, updateTaskDto: Prisma.TaskUncheckedUpdateManyInput, currentUserId: string) {
+
+    const taskExists = await this.dbService.task.findUnique({
+      where: { id },
       include: {
         boardListDetail: {
           include: {
-            boardDetail: true
+            boardDetail: true,
           }
-        },
+        }
       }
     });
+
+    if (!taskExists) throw new NotFoundException("Task not found");
+
+    let isOwnerOfBoard = taskExists.boardListDetail.boardDetail.userId === currentUserId;
+    let isAssignedToTask = taskExists.assignedTo === currentUserId
+
+    if (!isOwnerOfBoard && !isAssignedToTask)
+      throw new UnauthorizedException("You are not authorized to update this task");
+
+    // update done by current user
+    // The user who don't own the task can update assignee
+    if (isOwnerOfBoard) {
+      return this.dbService.task.update({
+        where: {
+          id: id,
+          boardListDetail: {
+            boardDetail: {
+              userId: currentUserId
+            }
+          }
+        },
+        data: {
+          ...updateTaskDto,
+          deadline: updateTaskDto.deadline && new Date(updateTaskDto.deadline as string)
+        },
+        include: {
+          boardListDetail: {
+            include: {
+              boardDetail: true
+            }
+          },
+        }
+      });
+    }
+
+
+    // update done by another user whom board is shared
+    // The user who don't own the task cannot update assignee
+    if (isAssignedToTask) {
+      return this.dbService.task.update({
+        where: {
+          id: id,
+          boardListDetail: {
+            boardDetail: {
+              userId: currentUserId
+            }
+          }
+        },
+        data: {
+          ...updateTaskDto,
+          deadline: updateTaskDto.deadline && new Date(updateTaskDto.deadline as string),
+          // The user who don't own the task cannot update assignee
+          assignedTo: currentUserId
+        },
+        include: {
+          boardListDetail: {
+            include: {
+              boardDetail: true
+            }
+          },
+        }
+      });
+    }
   }
 
-  remove(id: string, currentUserId: string) {
+  async remove(id: string, currentUserId: string) {
     const taskExists = this.findOne(id, currentUserId);
     if (!taskExists) throw new NotFoundException("Task not found");
     return this.dbService.task.delete({
